2e8cafa1416ce90a98eca0d972ddb2a14ff5e019
ALI <alex.ts.web@yandex.ru>
2020-08-04 09:23:17 +0300

    init: create file

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// import Layout from "./hoc/Layout/Layout";
+// import Quiz from './containers/Quiz/Quiz'
+
+class App extends Component {
+  render() {
+    return (
+      // <Layout>
+      //  <Quiz />
+      // </Layout>
+      <div>
+      <h1>Все ОК</h1>
+      <div style={{width: '100', height: '100', 'background-image': 'url(/assets/img/logo.svg)'}}></div>
+      </div>
+    );
+  }
+}
+
+export default App;
